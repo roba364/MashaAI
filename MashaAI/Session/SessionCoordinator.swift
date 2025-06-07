796576a5ba2cedc7426f7400d5597642c6616958
Sergey Borovkov <sergeyborovkov364@gmail.com>
2025-06-07 23:29:18 +0200

    [1] - removed trash

--- MashaAI/Session/SessionCoordinator.swift
+++ MashaAI/Session/SessionCoordinator.swift
@@ -2,25 +2,25 @@ import Foundation
 import MashaUIKit
 
 protocol SessionCoordinatorElementsFactory {
-  func voiceChatCoordinator(navigation: VoiceChatCoordinatorNavigation) -> VoiceChatCoordinator
-  func profileCoordinator(navigation: ProfileCoordinatorNavigation) -> ProfileCoordinator
+    func voiceChatCoordinator(navigation: VoiceChatCoordinatorNavigation) -> VoiceChatCoordinator
+    func profileCoordinator(navigation: ProfileCoordinatorNavigation) -> ProfileCoordinator
 }
 
 final class SessionCoordinator: ObservableObject {
-  let voiceChatCoordinator: VoiceChatCoordinator
-  let profileCoordinator: ProfileCoordinator
-
-  private let elementsFactory: SessionCoordinatorElementsFactory
-
-  init(
-    elementsFactory: SessionCoordinatorElementsFactory
-  ) {
-    self.elementsFactory = elementsFactory
-    self.voiceChatCoordinator = elementsFactory.voiceChatCoordinator(navigation: .init())
-    self.profileCoordinator = elementsFactory.profileCoordinator(navigation: .init())
-  }
-
-  func onAppear() async {
-    // Можно добавить логику, если нужно
-  }
+    let voiceChatCoordinator: VoiceChatCoordinator
+    let profileCoordinator: ProfileCoordinator
+    
+    private let elementsFactory: SessionCoordinatorElementsFactory
+    
+    init(
+        elementsFactory: SessionCoordinatorElementsFactory
+    ) {
+        self.elementsFactory = elementsFactory
+        self.voiceChatCoordinator = elementsFactory.voiceChatCoordinator(navigation: .init())
+        self.profileCoordinator = elementsFactory.profileCoordinator(navigation: .init())
+    }
+    
+    func onAppear() async {
+        // Можно добавить логику, если нужно
+    }
 }
